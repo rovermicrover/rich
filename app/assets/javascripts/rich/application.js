957abcf8a1401fb8dafb1b09ce10dba810602f83
Andrew Rove <ar@getupstream.com>
2012-05-24 19:40:07 -0500

    more fixing up of location of stuff. Clean up after move to assets for easier comptability with S3 sync gems and plugins.

--- app/assets/javascripts/rich/application.js
+++ app/assets/javascripts/rich/application.js
@@ -6,7 +6,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require fileuploader
+//= require rich/browser/fileuploader
 //= require rich/browser/extensions
 //= require rich/browser/uploader
 //= require rich/browser/filebrowser
